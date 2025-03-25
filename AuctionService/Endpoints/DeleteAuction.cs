329414444a549eebb6c0229e1fa74d6493c01531
FIRSTORY\nhatnguyen <nhatnguyen741@gmail.com>
2025-03-25 16:29:08 +0700

    Add Delete Auction Endpoint.

---
+++ AuctionService/Endpoints/DeleteAuction.cs
@@ -0,0 +1,20 @@
+﻿namespace AuctionService.Endpoints;
+
+internal sealed class DeleteAuction : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("api/auctions/{id:guid}", async Task<IResult> (Guid id, AuctionDbContext dbContext) =>
+        {
+            var auction = await dbContext.Auctions.FindAsync(id);
+
+            if (auction is null) return Results.NotFound();
+
+            dbContext.Auctions.Remove(auction);
+
+            var result = await dbContext.SaveChangesAsync() > 0;
+
+            return !result ? Results.BadRequest("Cannot delete auction") : TypedResults.Ok();
+        });
+    }
+}
